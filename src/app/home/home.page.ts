c51706b22bf53b91cf3f5170a62a9bd59416985f
Christoph Burnicki <christoph.burnicki@gmail.com>
2021-12-05 11:33:56 +0100

    initial commit: opening the gallery does not work on android 12

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  imageUrl: any;
 
   constructor() {}
 
+  async openGallery() {
+    const image = await Camera.getPhoto({
+      source: CameraSource.Photos,
+      quality: 90,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+    });
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    this.imageUrl = image.webPath;
+    console.log('IMAGE URL', this.imageUrl);
+
+    // Can be set to the src of an image now
+  }
 }
